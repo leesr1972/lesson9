0ebc8ac502005b47de2b65e98ef0fcbe70a74a59
Ли Сергей <sergey.r.lee@gmail.com>
2022-02-26 16:33:03 +0300

    ДЗ № 9 выпонено некорректно.

--- src/sky/pro/java/course1/hw9/MainHW9.java
+++ src/sky/pro/java/course1/hw9/MainHW9.java
@@ -11,9 +11,12 @@ public class MainHW9 {
         Book book2 = new Book("Золотая рыбка", aPushkin, 1992);
         Book book3 = new Book("Бородино", mLermontov, 2015);
         Book[] catalog = {book1, book2, book3};
-        System.out.println("В нашей библиотеке можно взять следующие книги:");
-        for (int i = 0; i < catalog.length; i++) {
-            System.out.println((i+1) + ". " + catalog[i]);
-        }
+        System.out.println("1. " + book1.getTitleBook() + ". " + lTolstoy.getAuthorName() + " " + lTolstoy.getAuthorSurname() + ". " + book1.getYearOfPublication() + " год.");
+        System.out.println("2. " + book2.getTitleBook() + ". " + aPushkin.getAuthorName() + " " + aPushkin.getAuthorSurname() + ". " + book2.getYearOfPublication() + " год.");
+        System.out.println("3. " + book3.getTitleBook() + ". " + mLermontov.getAuthorName() + " " + mLermontov.getAuthorSurname() + ". " + book3.getYearOfPublication() + " год.");
+        System.out.println();
+        book1.setYearOfPublication(2022);
+        System.out.println("У нас появилось новое издание книги " + book1.getTitleBook());
+        System.out.println(book1.getTitleBook() + ". " + lTolstoy.getAuthorName() + " " + lTolstoy.getAuthorSurname() + ". " + book1.getYearOfPublication() + " год.");
     }
 }
